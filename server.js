7a86ee554292be4643c14e27bb98adfa6e5db245
ArmaanTechDeveloper <kournarinder948@gmail.com>
2024-02-17 21:41:27 +0530

    added morgan to log the requests

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const path = require('path')
 
 const app = express();
 
 const PORT = 3000;
 
+
+app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname , 'public')))
 
 app.get('/' , (req , res) => {
